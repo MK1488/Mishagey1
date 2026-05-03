6b84850edae167b1e548ceb4e8c5f4ae06823dde
MK1488 <mkandakov721@gmail.com>
2026-05-03 19:20:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ from pygame import *
 from random import randint
 
 class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-       sprite.Sprite.__init__(self)
-       self.image = transform.scale(image.load(player_image), (size_x, size_y))
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+      sprite.Sprite.__init__(self)
+      self.image = transform.scale(image.load(player_image), (size_x, size_y))
+      self.speed = player_speed
+      self.rect = self.image.get_rect()
+      self.rect.x = player_x
+      self.rect.y = player_y
+    def reset(self):
+      window.blit(self.image, (self.rect.x, self.rect.y))
  
  
 class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
+    def update_r(self):
+      keys = key.get_pressed()
+      if keys[K_UP] and self.rect.y > 5:
+        self.rect.y -= self.speed
+      if keys[K_DOWN] and self.rect.y < win_height - 80:
+        self.rect.y += self.speed
 
-  def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
+    def update_l(self):
+      keys = key.get_pressed()
+      if keys[K_w] and self.rect.y > 5:
+        self.rect.y -= self.speed
+      if keys[K_s] and self.rect.y < win_height - 80:
+        self.rect.y += self.speed
          
 back = (200, 255, 255)
 win_width = 600
@@ -42,7 +42,7 @@ FPS = 60
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite(' tenis_ball.png', 200, 200, 4, 50, 50)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 font.init()
 font = font. Font (None, 35)
@@ -50,7 +50,7 @@ lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 speed_x = 3
-speedy = 3
+speed_y = 3
 
 while game:
   for e in event.get(): 
@@ -73,13 +73,14 @@ while game:
     finish = True
     window.blit(Lose1, (200, 200))
     game_over = True
-  if ball. rect.x > win width:
+  if ball.rect.x > win_width:
     finish = True
     window.blit(lose2, (200, 200))
     game_over = True
     racket1. reset()
-    racket2. reset)
-ball. reset
-display.update()
-cLock. tick(FPS)
+    racket2. reset()
+  ball.reset
+  display.update()
+  clock.tick(FPS)
+        
       
